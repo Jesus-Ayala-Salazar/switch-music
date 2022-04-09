2ff20ccec0127989bee0b4db28ea4fbbb3b8f4fd
Jesus <jaayala@uci.edu>
2022-04-08 18:15:19 -0700

    Processes JSON file with list of songs to download

--- create-mods.py
+++ create-mods.py
@@ -1,17 +1,22 @@
 import sys
 import requests
+import json
 
 def main():
     song_list = sys.argv[1]
     
     with open(song_list, 'rt') as song_list_file:
-        for song_ID in song_list_file:
-            url = f'https://smashcustommusic.net/nus3audio/{song_ID}'
-            file_dest = './DIRE.nus3audio'
+        data = json.load(song_list_file)
 
-    with open(file_dest, 'wb') as out_file:
-        content = requests.get(url, stream=True).content
-        out_file.write(content)
+    for entry in data:
+        song_name   = entry['name']
+        song_id     = entry['id']
+        file_dest   = f"./nus3audio-songs/{song_name}.nus3audio"
+        url         = f'https://smashcustommusic.net/nus3audio/{str(song_id)}'
+        content     = requests.get(url, stream=True).content
+        
+        with open(file_dest, 'wb') as out_file:    
+            out_file.write(content)
 
 if __name__ == '__main__':
     main()
